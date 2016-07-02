3c16a7ec46b0b6bd6f07d385dce259ddcaf5aabc
Aaron Dandy <aaron.dandy@live.com>
2016-07-01 21:38:02 -0700

    Created the main project

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#elif RELEASE
+[assembly: AssemblyConfiguration("Release")]
+#else
+[assembly: AssemblyConfiguration("Unknown")]
+#endif
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Hunspell")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
---
+++ src/Hunspell.NetCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Hunspell.NetCore")]
+[assembly: AssemblyDescription("A .NET port of Hunspell")]
+[assembly: CLSCompliant(true)]
