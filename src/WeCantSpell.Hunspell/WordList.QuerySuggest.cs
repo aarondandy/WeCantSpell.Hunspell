ac8083f1ddee58e04ffea04c3fe0da5c2882dc45
Aaron Dandy <aaron.dandy@live.com>
2018-09-03 23:01:39 -0700

    Adds custom enumerator for ngram suggestions

--- src/WeCantSpell.Hunspell/WordList.QuerySuggest.cs
+++ src/WeCantSpell.Hunspell/WordList.QuerySuggest.cs
@@ -1298,11 +1298,14 @@ namespace WeCantSpell.Hunspell
                     : string.Empty;
                 var isNonGermanLowercase = !Affix.IsGerman && capType == CapitalizationType.None;
 
+                var minAllowedLength = Math.Max(word.Length - 4, 0);
+                var maxAllowedLength = word.Length + 4;
+
                 foreach (var hpSet in WordList.GetNGramAllowedDetails(key =>
                 {
                     // skip it, if the word length different by 5 or
                     // more characters (to avoid strange suggestions)
-                    return Math.Abs(word.Length - key.Length) <= 4;
+                    return key.Length >= minAllowedLength && key.Length <= maxAllowedLength;
                 }))
                 {
                     var wordKeyLengthDifference = Math.Abs(word.Length - hpSet.Key.Length);
--- src/WeCantSpell.Hunspell/WordList.cs
+++ src/WeCantSpell.Hunspell/WordList.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using WeCantSpell.Hunspell.Infrastructure;
+using System.Collections;
 
 #if !NO_INLINE
 using System.Runtime.CompilerServices;
@@ -89,10 +90,7 @@ namespace WeCantSpell.Hunspell
 
         private FlagSet NGramRestrictedFlags { get; set; }
 
-        private IEnumerable<KeyValuePair<string, WordEntryDetail[]>> GetNGramAllowedDetails(Func<string, bool> rootFilter) =>
-            (NGramRestrictedDetails == null || NGramRestrictedDetails.Count == 0)
-            ? EntriesByRoot.Where(set => rootFilter(set.Key))
-            : GetAllNGramAllowedEntries(rootFilter);
+        private NGramAllowedEntries GetNGramAllowedDetails(Func<string, bool> rootKeyFilter) => new NGramAllowedEntries(this, rootKeyFilter);
 
         private Dictionary<string, WordEntryDetail[]> NGramRestrictedDetails { get; set; }
 
@@ -169,5 +167,95 @@ namespace WeCantSpell.Hunspell
 
                 return rootPair;
             });
+
+        private class NGramAllowedEntries : IEnumerable<KeyValuePair<string, WordEntryDetail[]>>
+        {
+            public NGramAllowedEntries(WordList wordList, Func<string, bool> rootKeyFilter)
+            {
+                this.wordList = wordList;
+                this.rootKeyFilter = rootKeyFilter;
+            }
+
+            private readonly WordList wordList;
+
+            private readonly Func<string, bool> rootKeyFilter;
+
+            public Enumerator GetEnumerator() => new Enumerator(wordList.EntriesByRoot, wordList.NGramRestrictedDetails, rootKeyFilter);
+
+            IEnumerator<KeyValuePair<string, WordEntryDetail[]>> IEnumerable<KeyValuePair<string, WordEntryDetail[]>>.GetEnumerator() => GetEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+            public class Enumerator : IEnumerator<KeyValuePair<string, WordEntryDetail[]>>
+            {
+                public Enumerator(Dictionary<string, WordEntryDetail[]> entriesByRoot, Dictionary<string, WordEntryDetail[]> nGramRestrictedDetails, Func<string, bool> rootKeyFilter)
+                {
+                    coreEnumerator = entriesByRoot.GetEnumerator();
+                    this.entriesByRoot = entriesByRoot;
+                    this.nGramRestrictedDetails = nGramRestrictedDetails;
+                    this.rootKeyFilter = rootKeyFilter;
+                    requiresNGramFiltering = nGramRestrictedDetails != null && nGramRestrictedDetails.Count != 0;
+                }
+
+                Dictionary<string, WordEntryDetail[]>.Enumerator coreEnumerator;
+                Dictionary<string, WordEntryDetail[]> entriesByRoot;
+                Dictionary<string, WordEntryDetail[]> nGramRestrictedDetails;
+                Func<string, bool> rootKeyFilter;
+                bool requiresNGramFiltering;
+
+                public KeyValuePair<string, WordEntryDetail[]> Current { get; private set; }
+
+                object IEnumerator.Current => Current;
+
+                public bool MoveNext()
+                {
+                    while (coreEnumerator.MoveNext())
+                    {
+                        var rootPair = coreEnumerator.Current;
+                        if (!rootKeyFilter(rootPair.Key))
+                        {
+                            continue;
+                        }
+
+                        if (requiresNGramFiltering)
+                        {
+                            if (nGramRestrictedDetails.TryGetValue(rootPair.Key.ToString(), out WordEntryDetail[] restrictedDetails))
+                            {
+                                if (restrictedDetails.Length != 0)
+                                {
+                                    WordEntryDetail[] filteredValues = rootPair.Value;
+                                    if (restrictedDetails.Length == rootPair.Value.Length)
+                                    {
+                                        continue;
+                                    }
+                                    else
+                                    {
+                                        filteredValues = filteredValues.Where(d => !restrictedDetails.Contains(d)).ToArray();
+                                    }
+
+                                    rootPair = new KeyValuePair<string, WordEntryDetail[]>(rootPair.Key, filteredValues);
+                                }
+                            }
+                        }
+
+                        Current = rootPair;
+                        return true;
+                    }
+
+                    Current = default;
+                    return false;
+                }
+
+                public void Reset()
+                {
+                    ((IEnumerator)coreEnumerator).Reset();
+                }
+
+                public void Dispose()
+                {
+                    coreEnumerator.Dispose();
+                }
+            }
+        }
     }
 }
