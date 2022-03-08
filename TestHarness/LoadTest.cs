5583c988d76a3011f2f787cabfabe2586c302ba8
Aaron <user@localhost.com>
2022-03-07 23:23:00 -0500

    Adds a new test harness for performance profiling

---
+++ TestHarness/LoadTest.cs
@@ -0,0 +1,21 @@
+﻿namespace WeCantSpell.Hunspell.TestHarness;
+
+public class LoadTest
+{
+    public static void LoadDictionary(string filePath)
+    {
+        var wordList = WordListReader.ReadFile(filePath);
+        Console.WriteLine($"Loaded {wordList.RootWords.Count()} roots");
+    }
+
+    public static void LoadAllDictionaries(string path)
+    {
+        var paths = Directory.GetFiles(path, "*.dic");
+
+        foreach (var filePath in paths)
+        {
+            var wordList = WordListReader.ReadFile(filePath);
+            Console.WriteLine($"Loaded {wordList.RootWords.Count()} roots from {filePath}");
+        }
+    }
+}
