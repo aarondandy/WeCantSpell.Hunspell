2845a5e12ff814fb28d493851cd6b13bc1fc1db6
Aaron <user@localhost.com>
2022-03-11 21:28:02 -0500

    Adds word check test harness

--- TestHarness/Program.cs
+++ TestHarness/Program.cs
@@ -6,6 +6,7 @@ var app = CoconaApp.Create();
 
 app.AddCommand("load", (string file) => LoadTest.LoadDictionary(file));
 app.AddCommand("load-all", (string path) => LoadTest.LoadAllDictionaries(path));
-app.AddCommand("suggest", (string dicFile, string wordFile) => SuggestTest.Suggest(dicFile, wordFile));
+app.AddCommand("check", (string dicFile, string wordFile) => CheckTest.Run(dicFile, wordFile));
+app.AddCommand("suggest", (string dicFile, string wordFile) => SuggestTest.Run(dicFile, wordFile));
 
 app.Run();
