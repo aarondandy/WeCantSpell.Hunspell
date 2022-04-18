5385b5845b61f9c2f5a4493bbbbe7e3b7dbcf7e6
Aaron <user@localhost.com>
2022-04-18 00:00:28 -0400

    Creates a reproduction of issue #40

--- TestHarness/Program.cs
+++ TestHarness/Program.cs
@@ -8,5 +8,6 @@ app.AddCommand("load", (string file) => LoadTest.LoadDictionary(file));
 app.AddCommand("load-all", (string path) => LoadTest.LoadAllDictionaries(path));
 app.AddCommand("check", (string dicFile, string wordFile) => CheckTest.Run(dicFile, wordFile));
 app.AddCommand("suggest", (string dicFile, string wordFile) => SuggestTest.Run(dicFile, wordFile));
+app.AddCommand("suggest-word", (string dicFile, string word) => SuggestWordTest.Run(dicFile, word));
 
 app.Run();
---
+++ TestHarness/SuggestWordTest.cs
@@ -0,0 +1,26 @@
+﻿namespace WeCantSpell.Hunspell.TestHarness;
+
+public class SuggestWordTest
+{
+    public static void Run(string dicFilePath, string word)
+    {
+        var wordList = WordListReader.ReadFile(dicFilePath);
+        const int wordLimit = 20;
+
+        var results = new List<int>(wordLimit);
+
+        Console.WriteLine($"Suggesting for word \"{word}\" {wordLimit} times");
+
+        for (var i = 0; i < wordLimit; i++)
+        {
+            var suggestions = wordList.Suggest(word);
+            results.Add(suggestions.Count());
+        }
+
+        Console.WriteLine("Results:");
+        foreach (var r in results)
+        {
+            Console.WriteLine($"{r}");
+        }
+    }
+}
