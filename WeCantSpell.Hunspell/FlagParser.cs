01a6702e24bdc051c66abf9e7a29b5417bbdf967
Aaron Dandy <user@localhost.com>
2025-02-22 23:11:45 -0800

    Reduces branching in flag parser

--- WeCantSpell.Hunspell/FlagParser.cs
+++ WeCantSpell.Hunspell/FlagParser.cs
@@ -7,21 +7,68 @@ namespace WeCantSpell.Hunspell;
 
 internal struct FlagParser
 {
-    public FlagParser()
-    {
-        Mode = FlagParsingMode.Char;
-        Encoding = Encoding.UTF8;
-    }
-
     public FlagParser(FlagParsingMode mode, Encoding encoding)
     {
+        _encoding = encoding;
+        _tryParseValue = FlagValue.TryParseAsChar;
+        _parseSet = FlagSet.ParseAsChars;
+        _parseValues = FlagValue.ParseAsChars;
         Mode = mode;
-        Encoding = encoding;
     }
 
-    public FlagParsingMode Mode { get; set; }
+    private Encoding _encoding;
+    private TryParseValueDelegate _tryParseValue;
+    private ParseValuesDelegate _parseValues;
+    private ParseSetDelegate _parseSet;
+    private FlagParsingMode _mode;
 
-    public Encoding Encoding { get; set; }
+    public Encoding Encoding
+    {
+        readonly get => _encoding;
+        set => _encoding = value;
+    }
+
+    public FlagParsingMode Mode
+    {
+        readonly get => _mode;
+        set
+        {
+            switch (value)
+            {
+                case FlagParsingMode.Char:
+                    _tryParseValue = FlagValue.TryParseAsChar;
+                    _parseValues = FlagValue.ParseAsChars;
+                    _parseSet = FlagSet.ParseAsChars;
+                    break;
+                case FlagParsingMode.Long:
+                    _tryParseValue = FlagValue.TryParseAsLong;
+                    _parseValues = FlagValue.ParseAsLongs;
+                    _parseSet = FlagSet.ParseAsLongs;
+                    break;
+                case FlagParsingMode.Num:
+                    _tryParseValue = FlagValue.TryParseAsNumber;
+                    _parseValues = FlagValue.ParseAsNumbers;
+                    _parseSet = FlagSet.ParseAsNumbers;
+                    break;
+                case FlagParsingMode.Uni:
+                    _tryParseValue = TryParseFlagAsUnicode;
+                    _parseValues = ParseFlagsInOrderAsUnicode;
+                    _parseSet = ParseFlagSetAsUnicode;
+                    break;
+                default:
+                    throwNotSupportedFlagMode();
+                    break;
+            }
+
+            _mode = value;
+
+#if !NO_EXPOSED_NULLANNOTATIONS
+            [System.Diagnostics.CodeAnalysis.DoesNotReturn]
+#endif
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            static void throwNotSupportedFlagMode() => throw new NotSupportedException("Flag mode is not supported");
+        }
+    }
 
     public readonly FlagValue ParseFlagOrDefault(ReadOnlySpan<char> text)
     {
@@ -29,47 +76,17 @@ internal struct FlagParser
         return result;
     }
 
-    public readonly bool TryParseFlag(ReadOnlySpan<char> text, out FlagValue value)
-    {
-        return Mode switch
-        {
-            FlagParsingMode.Char => FlagValue.TryParseAsChar(text, out value),
-            FlagParsingMode.Long => FlagValue.TryParseAsLong(text, out value),
-            FlagParsingMode.Num => FlagValue.TryParseAsNumber(text, out value),
-            FlagParsingMode.Uni => TryParseFlagAsUnicode(text, out value),
-            _ => fail(out value)
-        };
-
-        static bool fail(out FlagValue value)
-        {
-            value = default!;
-            return false;
-        }
-    }
+    public readonly bool TryParseFlag(ReadOnlySpan<char> text, out FlagValue value) => _tryParseValue(text, out value);
 
     private readonly bool TryParseFlagAsUnicode(ReadOnlySpan<char> text, out FlagValue value) =>
         FlagValue.TryParseAsChar(ReDecodeConvertedStringAsUtf8(text, Encoding), out value);
 
-    public readonly FlagValue[] ParseFlagsInOrder(ReadOnlySpan<char> text) => Mode switch
-    {
-        FlagParsingMode.Char => FlagValue.ParseAsChars(text),
-        FlagParsingMode.Long => FlagValue.ParseAsLongs(text),
-        FlagParsingMode.Num => FlagValue.ParseAsNumbers(text),
-        FlagParsingMode.Uni => ParseFlagsInOrderAsUnicode(text),
-        _ => ThrowNotSupportedFlagMode<FlagValue[]>()
-    };
+    public readonly FlagValue[] ParseFlagsInOrder(ReadOnlySpan<char> text) => _parseValues(text);
 
     private readonly FlagValue[] ParseFlagsInOrderAsUnicode(ReadOnlySpan<char> text) =>
         FlagValue.ParseAsChars(ReDecodeConvertedStringAsUtf8(text, Encoding));
 
-    public readonly FlagSet ParseFlagSet(ReadOnlySpan<char> text) => Mode switch
-    {
-        FlagParsingMode.Char => FlagSet.ParseAsChars(text),
-        FlagParsingMode.Long => FlagSet.ParseAsLongs(text),
-        FlagParsingMode.Num => FlagSet.ParseAsNumbers(text),
-        FlagParsingMode.Uni => ParseFlagSetAsUnicode(text),
-        _ => ThrowNotSupportedFlagMode<FlagSet>()
-    };
+    public readonly FlagSet ParseFlagSet(ReadOnlySpan<char> text) => _parseSet(text);
 
     private readonly FlagSet ParseFlagSetAsUnicode(ReadOnlySpan<char> text) =>
         FlagSet.ParseAsChars(ReDecodeConvertedStringAsUtf8(text, Encoding));
@@ -106,9 +123,7 @@ internal struct FlagParser
 #endif
     }
 
-#if !NO_EXPOSED_NULLANNOTATIONS
-    [System.Diagnostics.CodeAnalysis.DoesNotReturn]
-#endif
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    private static TResult ThrowNotSupportedFlagMode<TResult>() => throw new NotSupportedException("Flag mode is not supported");
+    private delegate bool TryParseValueDelegate(ReadOnlySpan<char> text, out FlagValue value);
+    private delegate FlagSet ParseSetDelegate(ReadOnlySpan<char> text);
+    private delegate FlagValue[] ParseValuesDelegate(ReadOnlySpan<char> text);
 }
