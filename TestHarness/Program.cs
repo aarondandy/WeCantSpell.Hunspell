0f1f1d67b95a2c84b65ea948c6c84442078b29a2
Aaron Dandy <user@localhost.com>
2025-08-29 16:45:14 -0700

    Improve test setup for #106

--- TestHarness/Program.cs
+++ TestHarness/Program.cs
@@ -9,7 +9,8 @@ app.AddCommand("load-all", (string path) => LoadTest.LoadAllDictionaries(path));
 app.AddCommand("check", (string dicFile, string wordFile) => CheckTest.Run(dicFile, wordFile));
 app.AddCommand("check-word", (string dicFile, string word) => CheckWordTest.Run(dicFile, word));
 app.AddCommand("suggest", (string dicFile, string wordFile) => SuggestTest.Run(dicFile, wordFile));
-app.AddCommand("suggest-word", (string dicFile, string word) => SuggestWordTest.Run(dicFile, word));
+app.AddCommand("suggest-word", (string dicFile, string word) => SuggestWordTest.RunIterationsSlow(dicFile, word));
+app.AddCommand("suggest-word-fast", (string dicFile, string word) => SuggestWordTest.RunIterationsFast(dicFile, word));
 app.AddCommand("issue88", SimpleIssueScenarios.Issue88);
 app.AddCommand("issue91", SimpleIssueScenarios.Issue91);
 
